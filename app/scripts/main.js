97d90fbb27412e6b09d504e7a70d54cd40c22b6d
Robert Hitt <rphitt1@gmail.com>
2014-05-14 15:59:54 -0400

    RESTFUL setup

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,45 @@
-console.log('\'Allo \'Allo!');
+$(document).ready({
+
+JamMasterP.init();
+
+});
+
+var JamMasterP= {
+	init: function() {
+		this.initStyling();
+		this.initEvents();
+	},
+
+	initiStyling: function() {
+	},
+
+	initEvents: function() {
+		// create playlist event
+		// add song to playlist
+		// pull up song tabs
+		//bring up modal
+	},
+
+	makeSetlist: function(){
+
+	},
+
+
+
+	addSong: function() {
+
+	},
+
+	removeSong: function() {
+
+	},
+
+	showTabs: function() {
+
+	},
+
+	updateSetlist: function() {
+
+	},
+
+}
