7dd67d1240dde8bf63780d1e204d2fdbc5bcce37
Shawn Leberknight <shawnleberknight@Shawns-MacBook-Pro.local>
2014-05-19 08:43:36 -0400

    fixed some styling with chords column

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,11 +2,6 @@
 
 $(document).ready(function() {
 
-$('.expand').on('click', function() {
-    $('.songs').toggle()
-  }
-);
-
 setListPro.init();
 
 });
@@ -29,86 +24,84 @@ var setListPro= {
 		// Delete song
 		$(".playlistsItem").on("click", ".deleteSong", this.removeSong);
 
+		$('.expand').on('click', function() {
+		    $('.songs').toggle();
+		    $('.chords').toggleClass('centerChords');
+		  }
+		);
+
 	},
 
 	inputSetList: function(data) {
     
-      $.ajaxSetup({
+      	$.ajaxSetup({
 			beforeSend: function(xhr) {
 				//had to do this to remove the Guitarparty-Api-Key otherwise post failed
 		}
-		})
+	})
 
-     $.ajax({
-              url:'http://tiy-fee-rest.herokuapp.com/collections/kesselrunners',
-              type:'POST',
-              data: data,
-              dataType: 'json',
-              error: function(data){
-                alert('U FAIL');
-              },
-              success: function(data) {
-                console.log('YA SUCCESS!');
+     	$.ajax({
+	        url:'http://tiy-fee-rest.herokuapp.com/collections/kesselrunners',
+	        type:'POST',
+	        data: data,
+	        dataType: 'json',
+	        error: function(data){
+	          alert('U FAIL');
+	        },
+	        success: function(data) {
+	          console.log('YA SUCCESS!');
 
-                   }
-               }); //end ajax
-
+	             }
+	        }); //end ajax
   },
 
-
-		getAjax : function(e){
+	getAjax : function(e){
 		e.preventDefault();
 
 		$.ajaxSetup({
 			beforeSend: function(xhr) {
 	        xhr.setRequestHeader('Guitarparty-Api-Key', API_KEY);
-		}
+			}
 		})
 
 		var API_KEY = '8b002700ba331e00ee2408de1d1a3da5c43382d7';
-
 		var html = '';
 		var songTitle = $('.songTitleInput').val();
 		var artist = $('.artistInput').val();
-
 		var fullURL = "http://api.guitarparty.com/v2/songs/";
 
 		$.ajax({
+			url: fullURL,
+			type: "GET",
+			dataType: "json",
+			data: {query: songTitle},
+			success: function(data, dataType, jqXHR){
+				console.log(data);
 
-		url: fullURL,
-		type: "GET",
-		dataType: "json",
-		data: {query: songTitle},
-		success: function(data, dataType, jqXHR){
-			console.log(data);
-
-			if (data.objects.length === 1){
-			
-			var name = data.objects[0].authors[0].name;
-			var title = data.objects[0].title;
-			var chords = data.objects[0].body_chords_html;
-			var someObj = {
-					name: name,
-					title: title,
-					chords: chords
-			};
+				if (data.objects.length === 1){
+				
+				var name = data.objects[0].authors[0].name;
+				var title = data.objects[0].title;
+				var chords = data.objects[0].body_chords_html;
+				var someObj = {
+						name: name,
+						title: title,
+						chords: chords
+				};
 
 			setListPro.inputSetList(someObj);
-				var objects;
 			setListPro.renderSong();
+			setListPro.renderChords();
 
-				$(".authorName").append(data.objects[0].authors[0].name);
-				$(".titleName").append(data.objects[0].title);
-				$(".chordsItem").append(data.objects[0].body_chords_html);
 
-			} else 
-				
-				alert("We don't have that ish yo, stop asking!");
-			 
-			
+				$(".authorName").html(data.objects[0].authors[0].name);
+				$(".titleName").html(data.objects[0].title);
+				$(".chordsItem").html(data.objects[0].body_chords_html);
+
+			} else 				
+				alert("We don't have that ish yo, stop asking!");		 			
 			}
 		});
-
 	},
 
 	render: function($el, template, data) {
@@ -150,27 +143,6 @@ var setListPro= {
 			}
 		});
 	},
-
-	appendTab : function(){
-		//getAjax
-		//rendertmpl....html
-	},
-	genSong : function(){
-		var artist=$(".artistInput").val();
-		var songTitle=$(".songTitleInput").val();
-		var newSong= new Song(artist, songTitle);
-		console.log(setlists);
-		return newSong;
-	},
-
-	addSong : function(){
-		$(".songForm").on("submit", function(event){
-			event.preventDefault();
-			setlists[0].songs.unshift(setListPro.genSong());
-			console.log(setlists);//HOW WILL IT KNOW WHICH ARRAY TO PUSH TO?
-		});
-	},
-
 	
 	removeSong: function() {
 		var $thisSong = $(this).closest("article");
@@ -188,11 +160,6 @@ var setListPro= {
 				setListPro.renderChords();
 			}
 		});
-	},
-
-
-	showTabs: function() {
-
 	},
 
 	updateSetlist: function() {
--- app/scripts/templates.js
+++ app/scripts/templates.js
@@ -2,7 +2,6 @@ Templates = {};
 
 Templates.songs = [
 	"<% _.each(obj, function(item, index, list) { %>",
-
 	"<article class=\"setListTemplate\" data-itemId=\"<%= item._id %>\">",
 	"<h4\"data-index=\"<%= index %>\"><h4>Artist: </4><%= item.name %></h4>",
 	"<h4\"data-index=\"<%= index %>\"><h4>Song: </4><%= item.title %></h4>",
@@ -10,18 +9,12 @@ Templates.songs = [
 	"</article>",
 	"<% }); %>"
 
-
 ].join("\n");
 
 Templates.showChord = [
 	"<% _.each(obj, function(item, index, list) { %>",
-
-	"<h4 class=\"thisTitle\"><%= item.title %></h4>",
+	"<h3 class=\"thisTitle\"><%= item.title %></h3>",
 	"<div class=\"songChords\" data-index=\"<%= index %>\"><%= item.chords %></div>",
 	"<% }); %>"
-
-
-
-
 
 ].join("\n");
